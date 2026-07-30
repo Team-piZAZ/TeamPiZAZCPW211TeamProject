5dc7cb5b9f391c3896c3c6fbeaf7bb5c5e871a61
Zac Kimball <z.kimball85@gmail.com>
2026-07-29 17:42:10 -0700

    Add AnimeDbContext and factory for EF Core setup
    Added AnimeDbContext with DbSets for Anime, Genre, and Studio, including entity relationships and seeding for studios and genres. Implemented AnimeDbContextFactory to enable design-time context creation, supporting configuration via appsettings.json or a default connection string.

---
+++ Database/AnimeDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace TeamPiZAZCPW211TeamProject.Database;
+
+public class AnimeDbContextFactory : IDesignTimeDbContextFactory<AnimeDbContext>
+{
+    public AnimeDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AnimeDbContext>();
+
+        string? connectionString = null;
+
+        if (File.Exists("appsettings.json"))
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        connectionString ??= "Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new AnimeDbContext(optionsBuilder.Options);
+
+    }
+}
+
+
